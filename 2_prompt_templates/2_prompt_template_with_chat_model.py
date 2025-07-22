2d7fc8636c6779e8662c477042e1039ce736ce86
itsareebalatif <itsareebalatif@gmail.com>
2025-07-22 15:13:46 +0500

    using ChatGroq(model=llama3-70b-8192) instead of open AI

--- 2_prompt_templates/2_prompt_template_with_chat_model.py
+++ 2_prompt_templates/2_prompt_template_with_chat_model.py
@@ -1,21 +1,26 @@
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 
 # Load environment variables from .env
+# load_dotenv()
+
+# # Create a ChatOpenAI model
+# model = ChatOpenAI(model="gpt-4o")
 load_dotenv()
 
-# Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+# # Create a ChatGroq model
+model = ChatGroq(model="llama3-70b-8192")  # or "llama3-70b-8192"
 
-# PART 1: Create a ChatPromptTemplate using a template string
-print("-----Prompt from Template-----")
-template = "Tell me a joke about {topic}."
-prompt_template = ChatPromptTemplate.from_template(template)
+# # PART 1: Create a ChatPromptTemplate using a template string
+# print("-----Prompt from Template-----")
+# template = "Tell me a joke about {topic}."
+# prompt_template = ChatPromptTemplate.from_template(template)
 
-prompt = prompt_template.invoke({"topic": "cats"})
-result = model.invoke(prompt)
-print(result.content)
+# prompt = prompt_template.invoke({"topic": "cats"})
+# result = model.invoke(prompt)
+# print(result.content)
 
 # PART 2: Prompt with Multiple Placeholders
 print("\n----- Prompt with Multiple Placeholders -----\n")
@@ -28,13 +33,13 @@ prompt = prompt_multiple.invoke({"adjective": "funny", "animal": "panda"})
 result = model.invoke(prompt)
 print(result.content)
 
-# PART 3: Prompt with System and Human Messages (Using Tuples)
-print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
-messages = [
-    ("system", "You are a comedian who tells jokes about {topic}."),
-    ("human", "Tell me {joke_count} jokes."),
-]
-prompt_template = ChatPromptTemplate.from_messages(messages)
-prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
-result = model.invoke(prompt)
-print(result.content)
+# # PART 3: Prompt with System and Human Messages (Using Tuples)
+# print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
+# messages = [
+#     ("system", "You are a comedian who tells jokes about {topic}."),
+#     ("human", "Tell me {joke_count} jokes."),
+# ]
+# prompt_template = ChatPromptTemplate.from_messages(messages)
+# prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
+# result = model.invoke(prompt)
+# print(result.content)
