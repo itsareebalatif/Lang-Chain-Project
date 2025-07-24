2ffcbae9c399e528c44931b05f6b297aa0617891
itsareebalatif <itsareebalatif@gmail.com>
2025-07-24 15:46:05 +0500

    we change the function to count words

--- 3_chains/3_chains_extended.py
+++ 3_chains/3_chains_extended.py
@@ -2,13 +2,14 @@ from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableLambda
+from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 
 # Load environment variables from .env
 load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+model = ChatGroq(model="llama3-70b-8192") 
 
 # Define prompt templates
 prompt_template = ChatPromptTemplate.from_messages(
@@ -20,8 +21,10 @@ prompt_template = ChatPromptTemplate.from_messages(
 
 # Define additional processing steps using RunnableLambda
 uppercase_output = RunnableLambda(lambda x: x.upper())
-count_words = RunnableLambda(lambda x: f"Word count: {len(x.split())}\n{x}")
+def count_words_fn(x: str) -> str:
+    return f"Word count: {len(x.split())}\n{x}"
 
+count_words = RunnableLambda(count_words_fn)
 # Create the combined chain using LangChain Expression Language (LCEL)
 chain = prompt_template | model | StrOutputParser() | uppercase_output | count_words
 
